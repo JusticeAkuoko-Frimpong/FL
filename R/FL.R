ff035626985ac9a935f92fd980205b00480f2751
JusticeAkuoko-Frimpong <jakuokof@umich.edu>
2023-12-10 23:37:04 -0500

    updates

--- R/FL.R
+++ R/FL.R
@@ -35,6 +35,8 @@
 #'Purchase2 = 20000 + 500*age2 + 0.05*income2 + 10*age2*income2/100000 + noise2
 #'data2 = data.frame(age = age2, income = income2, Purchase = Purchase2)
 #'summary2 = FL_local_summary(Purchase ~ age + income +income:age, data2)
+#'
+#'@importFrom stats model.matrix pt
 #'@export
 #'
 FL_local_summary = function(formula, data) {
